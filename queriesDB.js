9816e1eeffabd423d07222672e17ad28c6059e8a
Andi <ic20b050@technikum-wien.at>
2023-02-25 20:57:53 +0100

    database query works now

--- queriesDB.js
+++ queriesDB.js
@@ -1,7 +1,7 @@
 "use strict";
 
 //let connected = false;
-const { Pool } = require('pg')
+const Pool = require('pg').Pool
 
 const pool = new Pool({
     host: '127.0.0.1',
@@ -17,20 +17,20 @@ const getGemeinde = async (req, res) => {
     const client = await pool.connect()
 
     try {
-        await client.query(`SELECT gemeindename FROM public.gemeinde WHERE gkz=${req.query.id} LIMIT 1`, (err, res) => {
-            const resDB = err ? err.stack : res.rows[0].gemeindename;
-            res.json({gemeindename: resDB});
-            console.log({gemeindename: resDB})
-            //res.end()
+        const resDB = await client.query({
+            rowMode: 'array',
+            text: `SELECT gemeindename FROM public.gemeinde WHERE gkz=${req.query.id} LIMIT 1`,
         });
+
+        res.json({gemeindename: resDB.rows[0]});
     } catch (err) {
         console.log("Error occurred: " + err);
         res.statusMessage = err;
         res.status(500).end(); // on an error send http code 500 (= Internal Server Error) to the client
     }
-    // finally {
-    //     pool.release()
-    // }
+    finally {
+        await client.end();
+    }
 };
 
 module.exports = {
