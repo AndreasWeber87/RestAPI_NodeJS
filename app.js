efad42473264f1131a617d419ab3d83876d32c3b
Andi <ic20b050@technikum-wien.at>
2023-04-16 23:34:03 +0200

    complete crud

--- app.js
+++ app.js
@@ -8,7 +8,6 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const queries = require('./queries');
-const queriesDB = require('./queriesDB');
 
 const api = express();
 
@@ -18,12 +17,30 @@ api.use(bodyParser.urlencoded({extended: false}));
 api.use(bodyParser.json());
 
 api.get('/', queries.home);
-api.get('/getGemeinde', queriesDB.getGemeinde);
+api.post('/createTable', queries.createTable);
+api.post('/addStrasse', queries.addStrasse);
+api.put('/changeStrasse/:skz', queries.changeStrasse);
+api.get('/getStrasse', queries.getStrasse);
+api.delete('/deleteStrasse/:skz', queries.deleteStrasse);
 
 api.listen(8000, () => {
   console.log("Server started on port 8000...");
   console.log("");
   console.log("Possible calls:");
-  console.log("http://localhost:8000/");
-  console.log("GET: http://localhost:8000/getGemeinde?id=10101");
+  console.log("GET: http://localhost:8000/");
+  console.log("");
+  console.log("POST: http://localhost:8000/createTable");
+  console.log("	BODY:");
+  console.log("");
+  console.log("POST: http://localhost:8000/addStrasse");
+  console.log(" HEADER: Content-Type: application/json");
+  console.log("	BODY: {\"skz\":108711,\"strassenname\":\"Andromedastraße\"}");
+  console.log("");
+  console.log("PUT: http://localhost:8000/changeStrasse/108711");
+  console.log("	HEADER: Content-Type: application/json");
+  console.log("	BODY: {\"strassenname\":\"Andromedastraße2\"}");
+  console.log("");
+  console.log("GET: http://localhost:8000/getStrasse?skz=108711");
+  console.log("");
+  console.log("DELETE: http://localhost:8000/deleteStrasse/108711");
 });
--- queries.js
+++ queries.js
@@ -1,16 +1,126 @@
 "use strict";
 
+const Pool = require('pg').Pool;
+
+const pool = new Pool({
+    //host: '127.0.0.1',
+    host: '192.168.0.2', // container ip
+    port: 5432,
+    user: 'postgres',
+    password: 'xsmmsgbAMfIOIWPPBrsc',
+    database: 'ogd',
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+});
+
+// GET: http://localhost:8000/
 const home = (req, res) => {
     try {
         res.json({"message": "Hello World! I'm the NodeJS API."});
-    }
-    catch(err) {
+    } catch (err) {
         console.log(err.stack);
-        res.writeHead(500, {'Content-Type' : 'application/json'});
-        res.end(`{"message": "${err.stack}"}`.replace("\n", "\\n"));
+        res.sendStatus(500);
     }
-}
+};
+
+// POST: http://localhost:8000/createTable
+// BODY:
+const createTable = async (req, res) => {
+    try {
+        const sqlQuery = `DROP TABLE IF EXISTS public.strasse;
+
+CREATE TABLE IF NOT EXISTS public.strasse
+(
+    skz integer NOT NULL,
+    strassenname character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT strasse_pkey PRIMARY KEY (skz)
+)`;
+        pool.query(sqlQuery,
+            (error) => {
+                if (error) {
+                    throw error;
+                }
+                res.sendStatus(200);
+            });
+    } catch (err) {
+        console.log(err.stack);
+        res.sendStatus(500);
+    }
+};
+
+// POST: http://localhost:8000/addStrasse
+// HEADER: Content-Type: application/json
+// BODY: {"skz":108711,"strassenname":"Andromedastraße"}
+const addStrasse = async (req, res) => {
+    try {
+        pool.query("INSERT INTO public.strasse(skz, strassenname) VALUES ($1, $2);", [req.body.skz, req.body.strassenname],
+            (error) => {
+                if (error) {
+                    throw error;
+                }
+                res.sendStatus(200);
+            });
+    } catch (err) {
+        console.log(err.stack);
+        res.sendStatus(500);
+    }
+};
+
+// PUT: http://localhost:8000/changeStrasse/108711
+// HEADER: Content-Type: application/json
+// BODY: {"strassenname":"Andromedastraße2"}
+const changeStrasse = async (req, res) => {
+    try {
+        pool.query("UPDATE public.strasse SET strassenname=$1 WHERE skz=$2;", [req.body.strassenname, req.params.skz],
+            (error) => {
+                if (error) {
+                    throw error;
+                }
+                res.sendStatus(200);
+            });
+    } catch (err) {
+        console.log(err.stack);
+        res.sendStatus(500);
+    }
+};
+
+// GET: http://localhost:8000/getStrasse?skz=108711
+const getStrasse = async (req, res) => {
+    try {
+        pool.query("SELECT strassenname FROM public.strasse WHERE skz=$1 LIMIT 1;", [req.query.skz],
+            (error, results) => {
+                if (error) {
+                    throw error;
+                }
+                res.status(200).json(results.rows);
+            });
+    } catch (err) {
+        console.log(err.stack);
+        res.sendStatus(500);
+    }
+};
+
+// DELETE: http://localhost:8000/deleteStrasse/108711
+const deleteStrasse = async (req, res) => {
+    try {
+        pool.query("DELETE FROM public.strasse WHERE skz=$1;", [req.params.skz],
+            (error) => {
+                if (error) {
+                    throw error;
+                }
+                res.sendStatus(200);
+            });
+    } catch (err) {
+        console.log(err.stack);
+        res.sendStatus(500);
+    }
+};
 
 module.exports = {
     home,
+    createTable,
+    addStrasse,
+    changeStrasse,
+    getStrasse,
+    deleteStrasse
 }
